a60ba34d222b1f1b4e6de5dd158332633185a4aa
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-25 02:07:37 +0000

    Automatically backed up by Learn

--- thinking_space2.rb
+++ thinking_space2.rb
@@ -4,20 +4,6 @@ hill = [1,2,3,4,5,4,3,2,1]
 valley = [5,4,3,2,1,0,1,2,3,4,5]
 zig_zag = [500,4,1000,5,250]
 
-def find_max_value(array)
+def find_max_value(array) 
   
-  counter = 0 
   
-  while array[counter] && array[counter + 1] do
-    if array[counter] > array[counter + 1]
-      array[counter + 1] = array[counter]
-      array.shift
-      counter += 1 
-    else
-      puts "hmm"
-   end 
- end
-   
-end
-
-p find_max_value(scale_2) 
